ea18e0edd919ed2cd035b6062be60e646851fa91
ammosu <pang2258@gmail.com>
2024-11-11 00:22:24 +0800

    Update data.js

--- data.js
+++ data.js
@@ -9,6 +9,7 @@ const articlesData = [
     "publisher": "自由時報",
     "author": "張三",
     "fullText": "過去一年中，主要城市的房地產市場經歷了顯著的價格上漲，這引起了社會各界的廣泛關注。專家分析認為，這主要是由於經濟增長和市場需求增加所致，政府也在考慮採取措施來穩定市場。",
+    "expectedMarketTrend": "上漲",
     "imageUrl": "https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7"
   },
   {
@@ -19,6 +20,7 @@ const articlesData = [
     "publisher": "聯合報",
     "author": "李四",
     "fullText": "政府近日發布了一項新的住房政策，目的是為了控制房地產市場過熱的現象，確保居民的住房需求得到有效保障。這項政策包括對投機性購房行為的限制以及增加住房供應等多方面的措施。",
+    "expectedMarketTrend": "無法判斷",
     "imageUrl": "https://images.unsplash.com/photo-1501594907352-04cda38ebc29"
   },
   {
@@ -29,6 +31,7 @@ const articlesData = [
     "publisher": "經濟日報",
     "author": "王五",
     "fullText": "貸款利率的上調對房地產市場產生了直接影響，許多潛在購房者因為貸款成本的增加而推遲了購房計劃。專家表示，這可能會導致房地產市場需求的短期下降，但從長遠來看，這有助於市場的健康發展。",
+    "expectedMarketTrend": "下跌",
     "imageUrl": "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
   }
 ];
